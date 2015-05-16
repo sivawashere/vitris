c49347c2ecc1d0c71d8d0affac35b7616337bccd
Diana Chang <diana.chang05@gmail.com>
2015-05-16 17:28:37 -0400

    bs commit

--- app/src/main/java/com/seeminglycompetent/vitris/MainActivity.java
+++ app/src/main/java/com/seeminglycompetent/vitris/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 
+
+//hi
 /**
  * An {@link Activity} showing a tuggable "Hello World!" card.
  * <p/>

e34d99f24c1536e53a0a9e69de37d5a01b1d9c61
Anshula Gandhi <anshula.gandhi@gmail.com>
2015-05-17 01:39:50 -0400

    tapping works & up-down pan works

--- gyroimageview/build/generated/source/r/debug/com/neatocode/gyroimageview/R.java
+++ gyroimageview/build/generated/source/r/debug/com/neatocode/gyroimageview/R.java
@@ -12,8 +12,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int golden_gate_2934_x_640=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int image=0x7f020002;
+        public static final int ic_glass_logo=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_stop=0x7f020003;
+        public static final int image=0x7f020004;
     }
     public static final class id {
         public static final int image=0x7f050001;
@@ -24,5 +26,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int hello_world=0x7f040001;
+        public static final int title_activity_main=0x7f040002;
     }
 }
--- gyroimageview/src/main/java/com/neatocode/gyroimageview/ViewActivity.java
+++ gyroimageview/src/main/java/com/neatocode/gyroimageview/ViewActivity.java
@@ -1,8 +1,15 @@
 package com.neatocode.gyroimageview;
 
 import android.app.Activity;
+import android.content.Context;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.util.Log;
+import com.google.android.glass.touchpad.GestureDetector;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 
 import com.polites.android.GestureImageView;
 import com.polites.android.MoveAnimation;
@@ -17,20 +24,22 @@ import java.util.Arrays;
 public class ViewActivity extends Activity implements FilteredOrientationTracker.Listener {
 	
 	private static final int ANIMATION_DURATION_MS = 100;
-	private static final float GYRO_TO_X_PIXEL_DELTA_MULTIPLIER = 50;
-	
+	private static final float GYRO_TO_Y_PIXEL_DELTA_MULTIPLIER = 300;
+
 	private GestureImageView image;
 	private MoveAnimation moveAnimation;
 	private FilteredOrientationTracker tracker;
-		
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
+		mGestureDetector = createGestureDetector(this);
+
+		super.onCreate(savedInstanceState);
     	getWindow().setFormat(PixelFormat.RGB_565);
     	ScreenOn.run(this);
-    			
+
        	setContentView(R.layout.view_activity);
-    	image = (GestureImageView) findViewById(R.id.image);	
+    	image = (GestureImageView) findViewById(R.id.image);
     	moveAnimation = new MoveAnimation();
 		moveAnimation.setAnimationTimeMS(ANIMATION_DURATION_MS);
 		moveAnimation.setMoveAnimationListener(new MoveAnimationListener() {
@@ -39,7 +48,7 @@ public class ViewActivity extends Activity implements FilteredOrientationTracker
 				image.setPosition(x, y);
 				image.redraw();
 			}
-		});	
+		});
 		tracker = new FilteredOrientationTracker(this, this);
     }
 
@@ -48,7 +57,7 @@ public class ViewActivity extends Activity implements FilteredOrientationTracker
 		super.onResume();
 		tracker.onResume();
 	}
-	
+
 	@Override
 	protected void onPause() {
 		super.onPause();
@@ -59,28 +68,76 @@ public class ViewActivity extends Activity implements FilteredOrientationTracker
 	@Override
 	public void onUpdate(float[] aGyro, float[] aGyroSum) {
 		System.out.println(aGyro[0] + " " + aGyro[1] + " " + aGyro[2]);
-		final float yGyro = aGyro[1];
-		final float deltaX = GYRO_TO_X_PIXEL_DELTA_MULTIPLIER * yGyro;
-		animateTo(deltaX);		
+		final float xGyro = aGyro[0];
+		final float deltaY = GYRO_TO_Y_PIXEL_DELTA_MULTIPLIER * xGyro;
+		animateTo(deltaY);
 	}
 
 	// Animate to a given offset, stopping at the image edges.
-	private void animateTo(final float animationOffsetX) {
-		float nextX = image.getImageX() + animationOffsetX;
-		final int maxWidth = image.getScaledWidth();
-		final int leftBoundary = (-maxWidth / 2) + image.getDisplayWidth();
-		final int rightBoundary = (maxWidth / 2);
-		if ( nextX < leftBoundary ) {
-			nextX = leftBoundary;
-		} else if ( nextX > rightBoundary ) {
-			nextX = rightBoundary;
+	private void animateTo(final float animationOffsetY) {
+		float nextY = image.getImageY() + animationOffsetY;
+		final int maxHeight = image.getScaledHeight();
+		final int topBoundary = (-maxHeight / 2) + image.getDisplayHeight();
+		final int bottomBoundary = (maxHeight / 2);
+		if ( nextY < topBoundary ) {
+			nextY = topBoundary;
+		} else if ( nextY > bottomBoundary ) {
+			nextY = bottomBoundary;
 		}
 		moveAnimation.reset();
-//		moveAnimation.setTargetX(nextX);
-//		moveAnimation.setTargetY(image.getImageY());
+		moveAnimation.setTargetX(image.getImageX());
+		moveAnimation.setTargetY(nextY);
 
-		moveAnimation.setTargetY(nextX);
+//		moveAnimation.setTargetY(nextX);
 
 		image.animationStart(moveAnimation);
+	}
+
+//	@Override
+//	public boolean onKeyDown(int keyCode, KeyEvent event) {
+//		System.out.print("touched OUTSIDE");
+//		if(keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
+//			System.out.print("touched");
+//			return true;
+//		}
+//		return super.onKeyDown(keyCode, event);
+//	}
+
+	private GestureDetector mGestureDetector;
+
+
+
+	private GestureDetector createGestureDetector(Context context) {
+		GestureDetector gestureDetector = new GestureDetector(context);
+		gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
+			@Override
+			public boolean onGesture(Gesture gesture) {
+				if (gesture == Gesture.LONG_PRESS || gesture == Gesture.TAP) {
+					System.out.println("long press or tap");
+					openOptionsMenu();
+					return true;
+				} else if (gesture == Gesture.TWO_TAP) {
+					System.out.println("two tap");
+
+					return true;
+				} else if (gesture == Gesture.SWIPE_RIGHT) {
+					System.out.println("swipe right");
+					return true;
+				} else if (gesture == Gesture.SWIPE_LEFT) {
+					System.out.println("swipe left");
+					return true;
+				}
+				return false;
+			}
+		});
+		return gestureDetector;
+	}
+
+	@Override
+	public boolean onGenericMotionEvent(MotionEvent event) {
+		if (mGestureDetector != null) {
+			return mGestureDetector.onMotionEvent(event);
+		}
+		return false;
 	}
 }
